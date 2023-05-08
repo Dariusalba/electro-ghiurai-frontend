75730504c35eda058abdadd966dd0d51c4f28854
dariusalba <75644974+Dariusalba@users.noreply.github.com>
2023-05-08 18:26:19 +0300

    edited app.css

---
+++ src/src/pages/Account.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+
+function AccountInfo() {
+  const customerId = sessionStorage.getItem("customerId");
+  const [userInfo, setUserInfo] = useState({});
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:9191/customer/register/')
+      .then((response) => response.json())
+      .then((data) => setUserInfo(data))
+      .catch((error) => console.error("Error: ", error));
+
+    fetch(`http://localhost:9191/customer/order/${customerId}`)
+      .then((response) => response.json())
+      .then((data) => setOrders(data))
+      .catch((error) => console.error("Error: ", error));
+  }, [customerId]);
+
+  return (
+    <div>
+      <h1>Account Information</h1>
+      <h2>User Information:</h2>
+      <p>Username: {userInfo.username}</p>
+      <p>First Name: {userInfo.firstName}</p>
+      <p>Last Name: {userInfo.lastName}</p>
+      <h2>Current Orders:</h2>
+      {orders.map((order) => (
+        <div key={order.id}>
+          <p>Order ID: {order.id}</p>
+          <p>Title: {order.title}</p>
+          <p>Description: {order.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default AccountInfo;
---
+++ src/src/pages/RegisterForm.js
@@ -0,0 +1,135 @@
+import { useState } from "react";
+import "../App.css";
+import FormInput from "../components/forminput.js";
+import { Link } from 'react-router-dom';
+
+const RegisterForm = () => {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    countryOfOrigin:"",
+    password: "",
+    firstName:"",
+    lastName:"",
+    dateOfBirth:"",
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      errorMessage:
+        "Username should be 3-16 characters and shouldn't include any special character!",
+      label: "Username",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "firstName",
+      type: "text",
+      placeholder: "Enter your First Name",
+      errorMessage: "Please enter a valid First Name",
+      label: "First Name",
+      required: true,
+    },
+    {
+      id: 3,
+      name: "lastName",
+      type: "text",
+      placeholder: "Enter your Last Name",
+      errorMessage: "Please enter a valid Last Name",
+      label: "Last Name",
+      required: true,
+    },
+    {
+      id: 4,
+      name: "dateOfBirth",
+      type: "date",
+      placeholder: "Date of Birth",
+      label: "Date of Birth",
+      required: true,
+    },
+    {
+      id: 5,
+      name: "countryOfOrigin",
+      type: "country",
+      placeholder: "Country",
+      label: "Country",
+      errorMessage: "Please enter a valid country",
+      required: true,
+    },
+    {
+      id: 6,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      errorMessage: "Please enter a valid email address.",
+      label: "Email",
+      required: true,
+    },
+    {
+      id: 7,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      errorMessage:
+        "Password should be 8-16 characters long and should contain a letter, number and a special character.",
+      label: "Password",
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      required: true,
+    },
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  
+    fetch('http://localhost:9191/customer/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(values),
+  })
+  .then(response => {
+    if (response.status === 409) {
+      console.error('Username already exists');
+    } else if (response.status === 201) {
+      console.log('Data sent to server successfully:', response.json().then((file) => console.log(file)));
+    } else {
+      console.error('Error:', response.status);
+    }
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+  console.log(values);
+}
+  
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+
+  return (
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+        <h1>Register</h1>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+        <button onClick = {handleSubmit}>Register</button>
+        <p>Already have an account? <Link to="/login">Login</Link></p>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterForm;
