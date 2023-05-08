269e68ab9425cba85f32befd93a72fae3afb5432
dariusalba <paul.darius34@yahoo.com>
2023-05-08 19:54:51 +0300

    Update Order.js

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -19,60 +19,69 @@ function OrderForm() {
   };
 
   const handleAddRemark = () => {
-    setRemarks([...remarks, remarkValue]);
-    setValues({ ...values, remarks: [...values.remarks, remarkValue] });
+    const newRemark = { orderId: 0, description: remarkValue };
+    setRemarks([...remarks, newRemark]);
+    setValues({ ...values, remarks: [...values.remarks, newRemark] });
     setRemarkValue("");
   };
-  
 
   const handleSubmit = (e) => {
     e.preventDefault();
   
-    const newValues = { ...values, remarks };
+    const newOrder = {
+      customerId: customerId,
+      title: values.title,
+      description: values.description,
+      progress: 0,
+    };
   
-    fetch(`http://localhost:9191/customer/order/${customerId}`, {
+    fetch(`http://localhost:9191/customer/order`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(newValues),
+      body: JSON.stringify(newOrder),
     })
-    .then((response) => {
-      if (response.status === 201) {
-        console.log("Order placed successfully");
-        response.json().then((data) => {
-          const orderId = data.orderId;
-          remarks.forEach((remark) => {
-            fetch(`http://localhost:9191/customer/order/remark/${orderId}`, {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({ remark }),
-            })
-              .then((response) => {
-                if (response.status === 201) {
-                  console.log("Remark added successfully");
-                } else {
-                  console.error("Error: ", response.status);
-                }
+      .then((response) => {
+        if (response.status === 201) {
+          console.log("Order placed successfully");
+          response.json().then((data) => {
+            const orderId = data.orderId;
+            const remarksToSend = remarks.map((remark) => {
+              return { orderId: 0, description: remark };
+            });
+            remarksToSend.forEach((remark) => {
+              fetch(`http://localhost:9191/customer/order/remark/${orderId}`, {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(remark),
               })
-              .catch((error) => {
-                console.error("Error: ", error);
-              });
+                .then((response) => {
+                  if (response.status === 201) {
+                    console.log("Remark added successfully");
+                  } else {
+                    console.error("Error: ", response.status);
+                  }
+                })
+                .catch((error) => {
+                  console.error("Error: ", error);
+                });
+            });
           });
-        });
-      } else {
-        console.error("Error: ", response.status);
-      }
-    })
-    .catch((error) => {
-      console.error("Error: ", error);
-    });
+        } else {
+          console.error("Error: ", response.status);
+        }
+      })
+      .catch((error) => {
+        console.error("Error: ", error);
+      });
   
     setValues({ title: "", description: "", remarks: [] });
     setRemarks([]);
     console.log(values);
+    console.log(remarks);
   };
   
 
@@ -106,7 +115,7 @@ function OrderForm() {
         </div>
         <div>
           {remarks.map((remark, index) => (
-            <div key={index}>{remark}</div>
+            <div key={index}>{remark.description}</div>
           ))}
         </div>
         <div>
