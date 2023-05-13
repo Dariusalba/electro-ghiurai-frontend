44fffb6f137084073edbbfb1f3f95365ba233ba4
dariusalba <paul.darius34@yahoo.com>
2023-05-13 20:19:19 +0300

    Update ManagerDashboard.js

--- src/pages/ManagerDashboard.js
+++ src/pages/ManagerDashboard.js
@@ -1,10 +1,44 @@
+import React, { Component } from 'react';
 
-const ManagerDashboard = () => {
+class ManagerDashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFile: null
+    };
+  }
+
+  handleFileInputChange = (event) => {
+    this.setState({
+      selectedFile: event.target.files[0]
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('file', this.state.selectedFile);
+
+    fetch('http://localhost:9191/mng/finish/order/61', {
+      method: 'POST',
+      body: formData
+    })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  render() {
     return (
-        <div>
-            <h1>Manager Dashboard</h1>
-        </div>
-    )
+      <form onSubmit={this.handleSubmit}>
+        <input type="file" onChange={this.handleFileInputChange} />
+        <button type="submit">Upload</button>
+      </form>
+    );
+  }
 }
 
 export default ManagerDashboard;
