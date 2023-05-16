d8e62541d31e174c1bfdbf9fe055a3598cdd2890
dariusalba <paul.darius34@yahoo.com>
2023-05-16 04:09:45 +0300

    small change in files

---
+++ src/pages/LoginForm.jsx
@@ -0,0 +1,203 @@
+import React, { useState } from 'react';
+import "../App.css";
+import { Link } from 'react-router-dom';
+import FormInput from "../components/forminput.jsx";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const LoginForm = () => {
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      errorMessage:
+        "Username should be 3-16 characters and shouldn't include any special character!",
+      label: "Username",
+      pattern: "^[A-Za-z0-9_@#]{3,16}$",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      errorMessage:
+        "Password should be 8-16 characters long and should contain a letter, number and a special character.",
+      label: "Password",
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      required: true,
+    },
+  ];
+
+  const notifyInvalidUsernamePassword = () =>
+    toast.error('❌ Invalid username or password', {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+
+  const notifyUserNotFound = () =>
+    toast.error('❌ User not found', {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(JSON.stringify(values));
+    
+    fetch('http://localhost:9191/customer/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(values),
+    })
+      .then(response => {
+        if (response.status === 200) {
+          console.log('Login successful');
+          response.json().then(data => {
+            const customerId = data.customerId;
+            sessionStorage.setItem('customerId', customerId);
+            window.location.href = '/account';
+          });
+        } else if (response.status === 401) {
+          console.error('Invalid username or password');
+          notifyInvalidUsernamePassword();
+        } else if (response.status === 404) {
+          console.error('User not found');
+          notifyUserNotFound();
+        } else {
+          console.error('Error:', response.status);
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+  const handleSubmitManager = (e) => {
+    e.preventDefault();
+    console.log(JSON.stringify(values));
+
+    fetch('http://localhost:9191/mng/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(values),
+    })
+      .then(response => {
+        if (response.status === 200) {
+            console.log('Login successful');
+            window.location.href = '/manager/dashboard';
+        } else if (response.status === 401) {
+          console.error('Invalid username or password');
+          notifyInvalidUsernamePassword();
+        } else if (response.status === 404) {
+          console.error('User not found');
+          notifyUserNotFound();
+        } else {
+          console.error('Error:', response.status);
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const onUserTypeChange = (e) => {
+    setValues({ ...values, userType: e.target.value });
+  };
+
+  let x;
+  if (values.userType === "manager") {
+    x = handleSubmitManager
+  } else if (values.userType === "employee") {
+    x = handleSubmit
+  } else {
+    x = handleSubmit
+  }
+
+  return (
+    <div>
+      <div className="welcome2">
+        <Link to="/">
+          <h1 className='welcome-h1'>ElectroGhiurai</h1>
+        </Link>
+      </div>
+      <div className="app">
+        <form onSubmit={x}>
+          <h1 className='app-h1'>Login</h1>
+          {inputs.map((input) => (
+            <FormInput
+              key={input.id}
+              {...input}
+              value={values[input.name]}
+              onChange={onChange}
+            />
+          ))}
+          <div className='radio-group'>
+            <label>
+              <input
+                type="radio"
+                name="userType"
+                value="manager"
+                checked={values.userType === "manager"}
+                onChange={onUserTypeChange}
+              />
+              Manager
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="userType"
+                value="employee"
+                checked={values.userType === "employee"}
+                onChange={onUserTypeChange}
+              />
+              Employee
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="userType"
+                value="customer"
+                checked={values.userType !== "manager" && values.userType !== "employee"}
+                onChange={onUserTypeChange}
+              />
+              Customer
+            </label>
+          </div>
+          <button className='app-button'>Login</button>
+          <p>Don't have an account? <Link to="/register">Register now</Link>!</p>
+        </form>
+        <ToastContainer />
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ src/pages/Order.jsx
@@ -0,0 +1,153 @@
+import { useState } from "react";
+import '../components/Order.css';
+import { Link } from 'react-router-dom';
+
+const customerId = sessionStorage.getItem("customerId");
+
+function OrderForm() {
+  const [values, setValues] = useState({
+    customerId: `${customerId}`,
+    title: "",
+    description: "",
+    progress: 0,
+    remarks: [],
+  });
+
+  const [remarks, setRemarks] = useState([]);
+  const [remarkValue, setRemarkValue] = useState("");
+
+  const handleRemarkChange = (e) => {
+    setRemarkValue(e.target.value);
+  };
+
+  const handleAddRemark = () => {
+    const newRemark = { orderId: 0, description: remarkValue };
+    setRemarks([...remarks, newRemark]);
+    setValues({ ...values, remarks: [...values.remarks, newRemark] });
+    setRemarkValue("");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newValues = { ...values, remarks };
+    const remarksJson = JSON.stringify(remarks);
+
+    fetch(`http://localhost:9191/customer/order/${customerId}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newValues),
+    })
+      .then((response) => {
+        if (response.status === 201) {
+          console.log("Order placed successfully");
+          response.json().then((data) => {
+            const orderId = data.orderId;
+            fetch(`http://localhost:9191/customer/order/remark/${orderId}`, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: remarksJson,
+            })
+              .then((response) => {
+                if (response.status === 201) {
+                  console.log("Remarks added successfully");
+                } else {
+                  console.error("Error: ", response.status);
+                }
+              })
+              .catch((error) => {
+                console.error("Error: ", error);
+              });
+          });
+        } else {
+          console.error("Error: ", response.status);
+        }
+      })
+      .catch((error) => {
+        console.error("Error: ", error);
+      });
+
+    setValues({ title: "", description: "", remarks: [] });
+    setRemarks([]);
+  };
+
+  const accountRedirect = () => {
+    window.location.href = "/account";
+  };
+
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  return (
+    <div>
+      <div className="welcome1">
+        <Link to="/account">
+          <h1 className='welcome-h1'>ElectroGhiurai</h1>
+        </Link>
+      </div>
+      <div className="order-app">
+        <h1 className="app-h1">Create an order</h1>
+        <form onSubmit={handleSubmit} id="order-form">
+          <div className="form-input">
+            <label htmlFor="title">Title:</label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              value={values.title}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-input">
+            <label htmlFor="description">Description:</label>
+            <textarea
+              form="order-form"
+              id="description"
+              name="description"
+              className="description-input"
+              value={values.description}
+              onChange={handleChange}>
+            </textarea>
+          </div>
+          <div className="form-input">
+            <label htmlFor="remark">Remark:</label>
+            <div className="add-remark-input">
+              <input
+                type="text"
+                id="remark"
+                name="remark"
+                value={remarkValue}
+                onChange={handleRemarkChange}
+                className="remark-input"
+              />
+              <button type="button" onClick={handleAddRemark} className="app-button">
+              Add Remark
+              </button>
+              <label className="form-input">Remarks:</label>
+              <div className="remark-input">
+                {remarks.map((remark, index) => (
+                  <div className="remark" key={index}>
+                    *{remark.description};
+                  </div>
+                ))}
+              <br></br>
+              </div>
+            </div>
+          </div>
+        
+          <button type="submit" className="app-button">Submit</button>
+          <button type="button" onClick={accountRedirect} className="app-button">Go Back</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default OrderForm;
