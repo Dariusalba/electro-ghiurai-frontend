e6dbbf9965a90148b11aee5efca7f943348037ed
TutuGames38 <tutugames38@gmail.com>
2023-05-16 22:10:08 +0300

    updated EmployeeDashboard.jsx

--- src/pages/EmployeeDashboard.jsx
+++ src/pages/EmployeeDashboard.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 
 const employeeId = sessionStorage.getItem('employeeId');
 
@@ -14,29 +15,39 @@ function EmployeeDashboard() {
 
   return (
     <div>
-      <h1>Employee Dashboard</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Task Number</th>
-            <th>Task Type</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tasks.map((task, index) => (
-            <tr key={task.taskNr}>
-              <td>{index + 1}</td>
-              <td>{task.taskNr}</td>
-              <td>{task.taskType}</td>
-              <td>
-                <button>View</button>
-              </td>
+      <div className="welcome1">
+        <Link to="/">
+          <h1 className='welcome-h1'>ElectroGhiurai</h1>
+        </Link>
+        <Link to="/login">
+          <button className="welcome-button">Sign Out</button>
+        </Link>
+      </div>
+      <div className='app'>
+        <h1 className='app-h1'>Employee Dashboard</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Task Number</th>
+              <th>Task Type</th>
+              <th>Action</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {tasks.map((task, index) => (
+              <tr key={task.taskNr}>
+                <td>{index + 1}</td>
+                <td>{task.taskNr}</td>
+                <td>{task.taskType}</td>
+                <td>
+                  <button className='app-button'>View</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
