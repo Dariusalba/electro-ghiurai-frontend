666a70631b0f1c9320ba98966cbab6dad98a4cc7
dariusalba <paul.darius34@yahoo.com>
2023-05-16 22:26:11 +0300

    modal window to view spec task

--- src/pages/EmployeeDashboard.jsx
+++ src/pages/EmployeeDashboard.jsx
@@ -1,67 +1,90 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from "react-router-dom";
-import '../App.css'
 
 const employeeId = sessionStorage.getItem('employeeId');
 
 function EmployeeDashboard() {
-    const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([]);
+  const [selectedTask, setSelectedTask] = useState(null);
+  const [orderDetails, setOrderDetails] = useState(null);
 
-    useEffect(() => {
-        fetch(`http://localhost:9191/emp/tasks/${employeeId}`)
-            .then(response => response.json())
-            .then(data => setTasks(data))
-            .catch(error => console.log(error));
-    }, []);
+  useEffect(() => {
+    fetch(`http://localhost:9191/emp/tasks/${employeeId}`)
+      .then(response => response.json())
+      .then(data => setTasks(data))
+      .catch(error => console.log(error));
+  }, []);
 
-    const getTaskTypeName = (taskType) => {
-        switch (taskType) {
-            case 1:
-                return 'SPEC';
-            case 2:
-                return 'CODE REVIEW';
-            default:
-                return '';
-        }
-    };
+  const getTaskTypeName = (taskType) => {
+    switch (taskType) {
+      case 1:
+        return 'SPEC';
+      case 2:
+        return 'CODE';
+      default:
+        return 'REVIEW';
+    }
+  };
 
-    return (
-        <div>
-            <div className="welcome1">
-                <Link to="/">
-                    <h1 className='welcome-h1'>ElectroGhiurai</h1>
-                </Link>
-                <Link to="/login">
-                    <button className="welcome-button">Sign Out</button>
-                </Link>
-            </div>
-            <div className='app'>
-                <h1 className='app-h1'>Employee Dashboard</h1>
-                <table className='order-table'>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Task Number</th>
-                            <th>Task Type</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {tasks.map((task, index) => (
-                            <tr key={task.taskNr}>
-                                <td>{index + 1}</td>
-                                <td>{task.taskNr}</td>
-                                <td>{getTaskTypeName(task.taskType)}</td>
-                                <td>
-                                    <button className='app-button'>View</button>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+  const openModal = (task) => {
+    setSelectedTask(task);
+    fetch(`http://localhost:9191/customer/order/${task.internalOrder}`)
+      .then(response => response.json())
+      .then(data => setOrderDetails(data))
+      .catch(error => console.log(error));
+  };
+
+  const closeModal = () => {
+    setSelectedTask(null);
+    setOrderDetails(null);
+  };
+
+  return (
+    <div className='app'>
+      <h1>Employee Dashboard</h1>
+      <table className='order-table'>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Task Number</th>
+            <th>Task Type</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task, index) => (
+            <tr key={task.taskNr}>
+              <td>{index + 1}</td>
+              <td>{task.taskNr}</td>
+              <td>{getTaskTypeName(task.taskType)}</td>
+              <td>
+                <button onClick={() => openModal(task)}>View</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      {selectedTask && (
+        <div className="modal">
+          <div className="modal-content">
+            <h2>Task #{selectedTask.taskNr}</h2>
+            {orderDetails ? (
+              <div>
+                <p>Order #{selectedTask.internalOrder}</p>
+                <p>Order title: {orderDetails.title}</p>
+                <p>Order description: {orderDetails.description}</p>
+                <button>Open SpecDoc</button>
+                <button>Upload Spec</button>
+              </div>
+            ) : (
+              <p>Failed to load order details</p>
+            )}
+            <button onClick={closeModal}>Close</button>
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default EmployeeDashboard;
