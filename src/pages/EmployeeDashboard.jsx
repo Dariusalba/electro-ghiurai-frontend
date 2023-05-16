33362eef6f6a923a89502a3fe77a82afa5674730
dariusalba <paul.darius34@yahoo.com>
2023-05-16 22:15:08 +0300

    table design

--- src/pages/EmployeeDashboard.jsx
+++ src/pages/EmployeeDashboard.jsx
@@ -1,55 +1,67 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
+import '../App.css'
 
 const employeeId = sessionStorage.getItem('employeeId');
 
 function EmployeeDashboard() {
-  const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState([]);
 
-  useEffect(() => {
-    fetch(`http://localhost:9191/emp/tasks/${employeeId}`)
-      .then(response => response.json())
-      .then(data => setTasks(data))
-      .catch(error => console.log(error));
-  }, []);
+    useEffect(() => {
+        fetch(`http://localhost:9191/emp/tasks/${employeeId}`)
+            .then(response => response.json())
+            .then(data => setTasks(data))
+            .catch(error => console.log(error));
+    }, []);
 
-  return (
-    <div>
-      <div className="welcome1">
-        <Link to="/">
-          <h1 className='welcome-h1'>ElectroGhiurai</h1>
-        </Link>
-        <Link to="/login">
-          <button className="welcome-button">Sign Out</button>
-        </Link>
-      </div>
-      <div className='app'>
-        <h1 className='app-h1'>Employee Dashboard</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Task Number</th>
-              <th>Task Type</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tasks.map((task, index) => (
-              <tr key={task.taskNr}>
-                <td>{index + 1}</td>
-                <td>{task.taskNr}</td>
-                <td>{task.taskType}</td>
-                <td>
-                  <button className='app-button'>View</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+    const getTaskTypeName = (taskType) => {
+        switch (taskType) {
+            case 1:
+                return 'SPEC';
+            case 2:
+                return 'CODE REVIEW';
+            default:
+                return '';
+        }
+    };
+
+    return (
+        <div>
+            <div className="welcome1">
+                <Link to="/">
+                    <h1 className='welcome-h1'>ElectroGhiurai</h1>
+                </Link>
+                <Link to="/login">
+                    <button className="welcome-button">Sign Out</button>
+                </Link>
+            </div>
+            <div className='app'>
+                <h1 className='app-h1'>Employee Dashboard</h1>
+                <table className='order-table'>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Task Number</th>
+                            <th>Task Type</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {tasks.map((task, index) => (
+                            <tr key={task.taskNr}>
+                                <td>{index + 1}</td>
+                                <td>{task.taskNr}</td>
+                                <td>{getTaskTypeName(task.taskType)}</td>
+                                <td>
+                                    <button className='app-button'>View</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 }
 
 export default EmployeeDashboard;
