68dd2728b193aa2755b169a7638a94f6c22fb547
TutuGames38 <tutugames38@gmail.com>
2023-06-03 18:20:44 +0300

    changed plugins

--- src/components/DocEditor.jsx
+++ src/components/DocEditor.jsx
@@ -8,22 +8,38 @@ function DocEditor() {
     <div>
       <Editor
         onInit={(evt, editor) => (editorRef.current = editor)}
-        initialValue="<p>Aici scrii ce doresti tu!</p>"
+        initialValue="Aici scrii ce doresti tu!"
         init={{
           width: 1000,
           height: 700,
+          resize: false,
           plugins: [
-            'advlist autolink lists link image charmap print preview anchor',
-            'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table paste code help wordcount'
+            'advlist',
+            'autolink',
+            'lists',
+            'link',
+            'image',
+            'charmap',
+            'print',
+            'preview anchor',
+            'searchreplace',
+            'visualblocks',
+            'code',
+            'fullscreen',
+            'insertdatetime',
+            'media',
+            'table',
+            'paste',
+            'code',
+            'help',
+            'wordcount'
           ],
           toolbar:
             "undo redo | formatselect | " +
             "bold italic backcolor | alignleft aligncenter " +
             "alignright alignjustify | bullist numlist outdent indent | " +
-            "removeformat | help | table",
+            "removeformat |",
           selector: "textarea",
-          table_toolbar: "tableprops tabledelete | tableinsertrowbefore tableinsertrowafter tabledeleterow | tableinsertcolbefore tableinsertcolafter tabledeletecol",
           content_style:
             "body { background-color: rgb(163, 163, 163), font-family:Helvetica,Arial,sans-serif; font-size:14px }",
         }}
