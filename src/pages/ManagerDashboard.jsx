2f430ff827d53ca8789947bd760504e1ccd89767
dariusalba <paul.darius34@yahoo.com>
2023-06-04 18:23:40 +0300

    Update ManagerDashboard.jsx

--- src/pages/ManagerDashboard.jsx
+++ src/pages/ManagerDashboard.jsx
@@ -120,6 +120,24 @@ const ManagerDashboard = () => {
     }
   };
 
+  const handleDownloadCode = async () => {
+    try {
+      const response = await fetch(`http://localhost:9191/mng/download/code/${selectedOrderDetails.orderId}`);
+      const blob = await response.blob();
+      const url = URL.createObjectURL(blob);
+
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = `spec-${selectedOrderDetails.orderId}.zip`;
+
+      link.click();
+
+      URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const fetchOrderRemarks = async (orderId) => {
     try {
       const response = await fetch(`http://localhost:9191/customer/order/remark/${orderId}`);
@@ -565,6 +583,7 @@ const ManagerDashboard = () => {
                         </option>
                       ))}
                     </select>
+                    <button onclick={handleDownloadCode}>Download Code</button>
                     <input type="date" value={deadline} onChange={handleDeadlineChange} />
                     <button onClick={handleAssignReviewer}>Assign Reviewer</button>
                   </div>
