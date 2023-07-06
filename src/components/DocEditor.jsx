468b579c815a8419acfca8182c42dc47ab822856
TutuGames38 <tutugames38@gmail.com>
2023-07-06 20:54:47 +0300

    da

--- src/components/DocEditor.jsx
+++ src/components/DocEditor.jsx
@@ -8,7 +8,63 @@ function DocEditor() {
     <div>
       <Editor
         onInit={(evt, editor) => (editorRef.current = editor)}
-        initialValue="Aici scrii ce doresti tu!"
+        initialValue="
+        <div>
+          <h2>Sport Analysis App</h2>
+          <h4>Description: </h4>
+          <p>This app will have to analyze from various databases how much people do sports. It will have info about Football, Jogging, Cycling, etc.</p>
+          <br/>
+          <h4>Data Dictionary:</h4>
+          <table>
+            <tbody c>
+              <tr>
+                <td>Pui ceva lung aici, raule</td>
+                <td>Pui ceva lung aici, raule</td>
+                <td>Pui ceva lung aici, raule</td>
+              </tr>
+              <tr>
+                <td>Pui ceva lung aici, raule</td>
+                <td>Pui ceva lung aici, raule</td>
+                <td>Pui ceva lung aici, raule</td>
+              </tr>
+              <tr>
+                <td>Pui ceva lung aici, raule</td>
+                <td>Pui ceva lung aici, raule</td>
+                <td>Pui ceva lung aici, raule</td>
+              </tr>
+            </tbody>
+          </table>
+          <br/>
+          <h4>Function Declarations:</h4>
+          <table>
+            <tbody c>
+              <tr>
+                <td>Pui ceva lung aici, raule</td>
+                <td>Pui ceva lung aici, raule</td>
+                <td>Pui ceva lung aici, raule</td>
+              </tr>
+              <tr>
+                <td>Pui ceva lung aici, raule</td>
+                <td>Pui ceva lung aici, raule</td>
+                <td>Pui ceva lung aici, raule</td>
+              </tr>
+              <tr>
+                <td>Pui ceva lung aici, raule</td>
+                <td>Pui ceva lung aici, raule</td>
+                <td>Pui ceva lung aici, raule</td>
+              </tr>
+            </tbody>
+          </table>
+          <br/>
+          <h4>Algorithm implementation:</h4>
+          <p><code>#include&lt;iostream&gt;</code></p>
+          <p><code>using namespace std;</code></p>
+          <p><code>int main() {</code></p>
+          <p><code>&nbsp; &nbsp; &nbsp;cout&lt;&lt;Pui ceva lung aici, raule&lt;&lt;endl;</code></p>
+          <p><code>&nbsp; &nbsp; &nbsp;return 0;</code></p>
+          <p><code>}</code></p>
+        </div>
+        "
         init={{
           min_height: 712,
           resize: false,
